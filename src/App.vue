ad7ceca3abd3bb29f24e952ccec7a8bfa0b80bf8
lucashsu95 <lucashsu95@gmail.com>
2024-04-23 11:39:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+import ChildComp from './components/ChildComp.vue'
+
+const ary = ref([['a','b'],['a','c'],['b','d'],['b','e']])
+</script>
+
+<template>
+  <ChildComp title='a' :ary='ary' />
+</template>
---
+++ src/components/ChildComp.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed,ref } from 'vue'
+  
+const props = defineProps({
+  title:String,
+  ary: Array
+})
+
+const ary2 = ref(props.ary)
+const ary3 = computed(()=>{
+  const tmp = []
+  console.log(ary2.value)
+  for(const i of ary2.value){
+    if(i[0] == props.title){
+      tmp.push(i[1])
+    }
+  }
+  return tmp
+})
+</script>
+
+<template>
+  
+  {{title}}
+  {{ary3}}
+</template>
