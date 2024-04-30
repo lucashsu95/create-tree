b86b4072f20f9c1ca1122b3776f486200f1d34be
lucashsu95 <lucashsu95gmail.com>
2024-04-30 16:57:15 +0800

    Update File

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup>
-import { ref,computed } from 'vue'
-import ChildComp from './components/ChildComp.vue'
+import { ref, computed } from "vue";
+import ChildComp from "./components/ChildComp.vue";
 
-const ary = ref([['a','b'],['a','c'],['b','d'],['b','e'],['c','f'],['e','g'],['e','z'],['z','x'],['x','u'],['x','y'],['f','t'],['f','q'],['q','w'],['w','s'],['c','o']])
-//const ary = computed(()=>{
-//  ary = JSON.parse(text.value);
-//  return ary
-//})
-
+const text = ref(
+  "[['a','b'],['a','c'],['b','d'],['b','e'],['c','f'],['e','g'],['e','z'],['z','x'],['x','u'],['x','y'],['f','t'],['f','q'],['q','w'],['w','s'],['c','o']]"
+);
+const ary = computed(() => {
+  const array = JSON.parse(text.value.replace(/'/g, '"'));
+  return array;
+});
 </script>
 
 <template>
-  <!-- <textarea v-model="text" cols='50' rows='3'></textarea> -->
-  <ChildComp title='a' :ary='ary' />
+  <textarea v-model="text" cols="50" rows="3"></textarea>
+  <ChildComp title="a" :ary="ary" />
 </template>
